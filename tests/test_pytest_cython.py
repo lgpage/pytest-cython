09658471f3b8b2497c20f180550db46fa67190e8
Logan Page <page.lg@gmail.com>
2016-04-04 20:15:04 +0200

    add simple but useless test

--- tests/test_pytest_cython.py
+++ tests/test_pytest_cython.py
@@ -1,6 +1,22 @@
+import pytest_cython.plugin
 
-from cython import plugin
+
+SCRIPT = '''
+def foo(a, b):
+    """
+    >>> foo(1, 1)
+    2
+    """
+    return a + b
+'''
 
 
 def test_main():
-    assert plugin  # use your library here
+    assert pytest_cython.plugin  # use your library here
+
+
+def test_simple(testdir):
+    script = testdir.makepyfile(SCRIPT)
+    result = testdir.runpytest('-v', '--doctest-cython', script)
+    assert False
+    print result
