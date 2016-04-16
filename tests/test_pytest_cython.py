1d44cb1f609fd2a4dcb7d04d648898ee66447af5
Logan Page <page.lg@gmail.com>
2016-04-16 23:40:20 +0200

    updated test script

--- tests/test_pytest_cython.py
+++ tests/test_pytest_cython.py
@@ -1,14 +1,19 @@
+from __future__ import absolute_import
+
 import py
+import sys
 
 import pytest_cython.plugin
 
 PATH = py.path.local(__file__).dirpath()
 PATH = PATH.join('example-project', 'src', 'pypackage')
+sys.path.insert(0, str(PATH))
 
 
 def test_cython_ext_module(testdir):
-    module = 'cython_ext_module.so'
-    result = testdir.runpytest('-v', '--doctest-cython', str(PATH.join(module)))
+    module = PATH.listdir(fil='cython_ext_module*.so')[0]
+    assert module.check()
+    result = testdir.runpytest('-vv', '--doctest-cython', str(module))
     result.stdout.fnmatch_lines([
         "*Eggs.__init__ *PASSED",
         "*Eggs.blarg*PASSED",
@@ -18,8 +23,9 @@ def test_cython_ext_module(testdir):
 
 
 def test_wrap_c_ext_module(testdir):
-    module = 'wrap_c_ext_module.so'
-    result = testdir.runpytest('-v', '--doctest-cython', str(PATH.join(module)))
+    module = PATH.listdir(fil='wrap_c_ext_module*.so')[0]
+    assert module.check()
+    result = testdir.runpytest('-vv', '--doctest-cython', str(module))
     result.stdout.fnmatch_lines([
         "*sqr*PASSED",
     ])
@@ -27,8 +33,9 @@ def test_wrap_c_ext_module(testdir):
 
 
 def test_wrap_cpp_ext_module(testdir):
-    module = 'wrap_cpp_ext_module.so'
-    result = testdir.runpytest('-v', '--doctest-cython', str(PATH.join(module)))
+    module = PATH.listdir(fil='wrap_cpp_ext_module*.so')[0]
+    assert module.check()
+    result = testdir.runpytest('-vv', '--doctest-cython', str(module))
     result.stdout.fnmatch_lines([
         "*sqr*PASSED",
     ])
@@ -36,8 +43,9 @@ def test_wrap_cpp_ext_module(testdir):
 
 
 def test_pure_py_module(testdir):
-    module = 'pure_py_module.py'
-    result = testdir.runpytest('-v', '--doctest-cython', str(PATH.join(module)))
+    module = PATH.listdir(fil='pure_py_module*.py')[0]
+    assert module.check()
+    result = testdir.runpytest('-vv', '--doctest-cython', str(module))
     result.stdout.fnmatch_lines([
         "*Eggs.__init__*PASSED",
         "*Eggs.foo*PASSED",
