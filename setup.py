951a117ee0dd62da772a077ad9d87ecd7dc9a3d1
Logan Page <page.lg@gmail.com>
2024-03-05 09:26:33 +0200

    chore: pin pytest>=8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'pytest', 'py.test', 'cython', 'doctest',
     ],
     install_requires=[
-        'pytest>=4.6,<8',
+        'pytest>=8',
     ],
     entry_points={
         'pytest11': [
